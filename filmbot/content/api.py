303471ae402b15b1cc931d118672bc5696816c86
DobryiMrak <kusok.apelbsina@hotmail.com>
2021-08-15 22:27:41 +0500

    Перекинул представления из вьюшек в апи

---
+++ filmbot/content/api.py
@@ -0,0 +1,43 @@
+from rest_framework import viewsets
+from .serializers import *
+from .models import Dictribution, Film, Activity, Message
+from fuzzywuzzy import process
+
+
+class DictributionViewSet(viewsets.ModelViewSet):
+    queryset = Dictribution.objects.all()
+    serializer_class = DictributionSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        query_params = self.request.query_params
+        unique_name = query_params.get('unique_name', None)
+        if unique_name:
+            message, created = Message.objects.get_or_create(
+                unique_name=unique_name,
+                defaults={'text': f'Текст для {unique_name} не уставлен!'}
+            )
+            return [message]
+        return Message.objects.all()
+
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
+
+
+class FilmViewSet(viewsets.ModelViewSet):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
+
+    def get_queryset(self):
+        query_params = self.request.query_params
+        search_data = query_params.get('search_data', None)
+        if search_data:
+            films = [film[0] for film in process.extract(search_data, Film.objects.all())]
+            return films
+        return Film.objects.all()
